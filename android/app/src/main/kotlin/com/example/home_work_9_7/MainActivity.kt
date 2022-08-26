eb4221a3d3069527dfe19d8dd9b4b777a55af5e1
Vlarimit N. Yatsuk <yatsukvn@gmail.com>
2022-08-26 12:42:34 +0600

    init commit

---
+++ android/app/src/main/kotlin/com/example/home_work_9_7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.home_work_9_7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inherited Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Homework provider',
+          ),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
